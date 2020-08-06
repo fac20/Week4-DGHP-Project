0c062fc958bfb86fb56426cd906e0ad423b51487
Terrence <netceer112@gmail.com>
2020-08-06 18:51:33 +0100

    add dark mode and switch for light bulb

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 const rope = document.querySelector(".rope");
 const bulb = document.querySelector("#form-bulb");
+const body = document.querySelector("body");
 
 rope.addEventListener("click", () => {
   bulb.classList.toggle("hideBulb");
+  body.classList.toggle("dark-background");
 })
 
 const buttonToMain = document.querySelector("#button-to-main");
