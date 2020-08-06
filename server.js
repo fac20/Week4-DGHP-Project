9b42fa72837f8e85447c9316af97aae9eb0ff493
Terrence <netceer112@gmail.com>
2020-08-06 18:07:29 +0100

    fix: unbind specific listening port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const router = require("./router");
 
-const PORT = 5000;
+// const PORT = 5000;
 
 const server = http.createServer(router);
-server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+server.listen(process.env.PORT || 5000, () => console.log(`Listening on http://localhost:${PORT}`));
