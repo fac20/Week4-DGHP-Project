0785bd946f29173422e48d87ffeb092659ec81dd
Terrence <netceer112@gmail.com>
2020-08-06 18:09:46 +0100

    fix: remove  from console log

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const router = require("./router");
 // const PORT = 5000;
 
 const server = http.createServer(router);
-server.listen(process.env.PORT || 5000, () => console.log(`Listening on http://localhost:${PORT}`));
+server.listen(process.env.PORT || 5000, () => console.log(`Listening on http://localhost:`));
